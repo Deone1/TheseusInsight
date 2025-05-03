8fa22537d53689eb5996dc61632d8fa53d6c4391
M Chimiste <37678080+M-Chimiste@users.noreply.github.com>
2025-05-03 17:47:35 -0400

    Update arxiv.py

--- theseus_insight/data_processing/arxiv.py
+++ theseus_insight/data_processing/arxiv.py
@@ -99,6 +99,7 @@ class ArxivDataProcessor:
         )
         records = harvester.harvest()
         data_df = harvester.to_dataframe()
+        data_df = data_df.rename(columns={"created": "date"})
         return data_df
             
             
